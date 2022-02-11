a56bf5e03e323a39f71da70848175862df9e97a3
AZIZ <a.matyaqubov1471@gmail.com>
2022-02-11 08:47:05 +0100

    add textUpdate and makeBold functions

---
+++ script.js
@@ -0,0 +1,11 @@
+const updateText = () => {
+    let yourText = document.querySelector('#exampleFormControlTextarea1').value   
+    document.querySelector('#text-output').innerText = yourText
+}
+
+
+const makeBold = () => {
+    let textBold = document.querySelector('#text-output')
+    textBold.classList.add("bold");
+}
+
